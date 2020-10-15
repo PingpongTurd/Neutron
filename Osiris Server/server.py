1d5385d496f86c08f58b134f18cb1682a75be2c5
ElectronDevDude <ElectronDev@protonmail.com>
2020-10-15 18:35:21 +0100

    Server complete
    in theory the server code should work, will probably have a socekt error somewhere but i need to write some test code

--- Osiris Server/server.py
+++ Osiris Server/server.py
@@ -1,1 +1,40 @@
 import socket
+import json
+import threading
+#data sent to server should be a list of 2, 1st value contains name, 2nd with a score!
+def receive():
+    while True: 
+        conn, addr = server.accept() 
+        clients[conn] = {}
+        clients[conn]['CLIENT'] = conn
+        clients[conn]['IP'] = addr 
+        clients[conn]['DATA'] = list(conn.recv(8000).decode('utf-8'))
+        if len(list(clients[conn]['DATA'])) != 2:
+            clients[conn]["CLIENT"].close()
+            del clients[conn]
+        else:
+            data={clients[conn]['DATA'][0]:clients[conn]['DATA'][1]}
+            with open('glb.json',"r") as f:
+                clb = json.load(f)
+            clb.append(data)
+            with open('glb.json','w') as f:
+                json.dump(clb,f)
+            clients[conn]["CLIENT"].close()
+            del clients[conn]
+
+
+while True:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ip = ""
+    port = 8888
+    try: 
+        server.bind((ip,port))
+    except socket.error as e:
+        print(str(e)) 
+    server.listen() 
+
+    clients = {}
+
+    a = threading.Thread(target=receive)
+    a.start()
+    a.join()
